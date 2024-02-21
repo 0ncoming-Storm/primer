5d6e87357df74e25208862b5db32922e99e9629d
0ncoming-Storm <157441211+0ncoming-Storm@users.noreply.github.com>
2024-02-20 18:09:57 -0700

    Refactor bubblesort algorithm in bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -11,29 +11,29 @@ def swap_in_list(input_list,index1,index2):
     '''
     This function swaps the value at index1 with the value at index2
     '''
+    swap = True
     temp = input_list[index2]
     input_list[index2] = input_list[index1]
     input_list[index1] = temp
-    return input_list
+    return swap, input_list
 
-def sort(unsorted_list):
+def sort(list):
     '''
     This function sorts the list with bubblesort
     '''
-    for j in range(len(unsorted_list)):
-
-        for i in range(len(unsorted_list)):
-            if unsorted_list[i] > unsorted_list[(i+1)]:
-                swap_in_list(unsorted_list,i,(i+1))
+    for j in range(len(list)):
+        swap = False
+        for i in range(len(list)-1):
+            if list[i] > list[(i+1)]: # Swap the elements if they are in the wrong order
+                swap = swap_in_list(list,i,(i+1))
             i += 1
+        if swap == False:  # If no swaps were made in the inner loop, the list is already sorted
+            return list
         j =+ 1
-
-    return sorted_list
 
 def main():
     unsorted_list = make_list()
     sorted_list = sort(unsorted_list)
     print(sorted_list)
-
 
 main()
