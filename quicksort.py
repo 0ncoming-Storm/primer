e0e6cc733f7aab5631f6d3ebbd1edf7bdf392b02
0ncoming-Storm <157441211+0ncoming-Storm@users.noreply.github.com>
2024-02-23 14:31:35 -0700

    Add insertion sort algorithm and measure execution times

--- quicksort.py
+++ quicksort.py
@@ -1,9 +1,19 @@
-from bubblesort import swap_in_list, make_list, bubblesort
+# Importing necessary functions and modules
+from bubblesort import swap_in_list, bubblesort
 import timeit
 import random
 
-
+# Defining the insertion_sort function
 def insertion_sort(input_list):
+    """
+    Sorts a list in ascending order using the insertion sort algorithm.
+
+    Parameters:
+    input_list (list): The list to be sorted.
+
+    Returns:
+    list: The sorted list.
+    """
     i = 1
     while i < len(input_list):
         j = i
@@ -13,29 +23,33 @@ def insertion_sort(input_list):
         i += 1
     return input_list
 
+# Getting the desired length of the test list from the user
+length = int(input("How long do you want the test list to be: "))
 
-
-lenght = int(input("How long do you want the test list to be: "))
-
-input_list = [random.randint(1, 100) for _ in range(lenght)]
-
+# Lists to store the execution times of bubble sort and insertion sort
 bubble_times = []
 insertion_times = []
 
+# Running the sorting algorithms multiple times to get average execution times
 for _ in range(10):
-    input_list = [random.randint(1, 100) for _ in range(50)]
+    # Generating a random test list
+    input_list = [random.randint(1, 100) for _ in range(length)]
 
-    bubbletime = timeit.timeit(stmt='bubblelist = bubblesort(input_list)', globals=globals(), number=1)
-    insertion_time = timeit.timeit(stmt='insertion_list = insertion_sort(input_list)', globals=globals(), number=1)
+    # Measuring the execution time of bubble sort
+    bubble_time = timeit.timeit(stmt='bubblelist = bubblesort(input_list)', globals=globals())
 
-    bubble_times.append(bubbletime)
+    # Measuring the execution time of insertion sort
+    insertion_time = timeit.timeit(stmt='insertion_list = insertion_sort(input_list)', globals=globals())
+
+    # Appending the execution times to the respective lists
+    bubble_times.append(bubble_time)
     insertion_times.append(insertion_time)
 
+# Calculating the average execution times
 average_bubble_time = sum(bubble_times) / len(bubble_times)
 average_insertion_time = sum(insertion_times) / len(insertion_times)
-    
 
-
+# Printing the results
 print("For a random list length", len(input_list))
-print("Average bubblesort time:", average_bubble_time)
-print("Average insertion sort time:", average_insertion_time)
+print("Average bubblesort time: ", round(average_bubble_time, 4), " seconds")
+print("Average insertion sort time", round(average_insertion_time, 4), " seconds")
