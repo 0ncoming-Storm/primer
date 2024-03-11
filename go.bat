33c764bb2a252bf6ef27c762a00aa86811ffd6d5
0ncoming-Storm <157441211+0ncoming-Storm@users.noreply.github.com>
2024-03-11 16:42:13 -0600

    .
    .

---
+++ go.bat
@@ -0,0 +1,3 @@
+@echo off 
+PATH=%PATH%;c:\mingw\bin;..\; 
+echo %PATH% 
---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Stream declarations
+using namespace std; 
+int main(void) { 
+cout << "Hello World from MinGW C++" << endl; 
+return 0; 
+}
